bfd0baf4ae49386e4085c9ff2fe3e96ee4ebaca2
D'angelo <dangeloshakur@gmail.com>
2020-04-11 16:57:24 -0500

    first commit, discussion assignment complete, reading exercises complete

---
+++ programming_fundamentals/week_1/learning_journal_unit_1_cs1101.md
@@ -0,0 +1,5 @@
+
+# Learning Journal Unit 1 CS_1101
+
+### Student: D'angelo M. Shakur
+### Date: 4/11/20
---
+++ programming_fundamentals/week_1/think_python_chapter_1_exercises_unit_1_cs1101.py
@@ -0,0 +1,30 @@
+
+#                                 #
+# Chapter 1 Exercise Section 1.1. #
+#                                 #
+
+# print(6 # 1.1. 1 RESULT: SyntaxError unexpected EOF while parsing
+
+# print 6) # 1.1. 1 RESULT: SyntaxError: Missing parentheses in call to 'print'. Did you mean print(x)?
+
+# print('string) # 1.1. 2 RESULT: SyntaxError: EOL while scanning string literal
+
+# print(string') # 1.1. 2 RESULT: SyntaxError: EOL while scanning string literal
+
+# print(2++2) # 1.1. 3 RESULT: 4 # ++ is an increment operator the mathematical operation went on ignoring the second + it seems
+
+# print(03) # 1.1. 4 RESULT: SyntaxError: invalid token
+
+# print(4 4) # 1.1. 5 RESULT: SyntaxError: invalid token
+
+#                                 #
+# Chapter 1 Exercise Section 1.2. #
+#                                 #
+
+# print((42 * 60) + 42) # RESULT: 2562
+ 
+# print(10 / 1.61) # RESULT: 6.211180124223602
+
+# print(60 / ((2562 / 6.211180124223602) / 60)) # RESULT: 8.727653570337614
+
+# SIDE NOTE: When you run, you do not measure your speed in MPH, your measure your speed in minutes per mile/kilometer
